3bdf5c845e6bbf60361f7e1894ed3b47f2ce0fe7
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2023-01-07 20:13:16 +0100

    email data added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+const mg = require('nodemailer-mailgun-transport');
 require('dotenv').config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -16,6 +18,49 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zvyzgm1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function sendBookingEmail(booking) {
+ const { email, treatment, appointmentDate, slot } = booking;
+
+ const auth = {
+  auth: {
+    api_key: process.env.EMAIL_SEND_KEY,
+    domain: process.env.EMAIL_SEND_DOMAIN
+  }
+}
+
+const transporter = nodemailer.createTransport(mg(auth));
+
+ // let transporter = nodemailer.createTransport({
+ //  host: 'smtp.sendgrid.net',
+ //  port: 587,
+ //  auth: {
+ //   user: "apikey",
+ //   pass: process.env.SENDGRID_API_KEY
+ //  }
+ // });
+
+ transporter.sendMail({
+  from: "smraju1234@gmail.com", // verified sender email
+  to: email, // recipient email
+  subject: `Your appointment for ${treatment} is confirmed`, // Subject line
+  text: "Hello world!", // plain text body
+  html: `
+  <h3>Your appointment is confirmed</h3>
+  <div>
+   <p> Your appointment for treatment: ${treatment}</p>
+   <p> Please visit us on ${appointmentDate} at ${slot}</p>
+   <p> Thanks from Doctors Portal.</p>
+  </div>
+  `, // html body
+ }, function (error, info) {
+  if (error) {
+   console.log('Email send error', error);
+  } else {
+   console.log('Email sent: ' + info.response);
+  }
+ });
+}
 
 function verifyJWT(req, res, next) {
 
@@ -176,7 +221,9 @@ async function run() {
    }
 
    const result = await bookingsCollection.insertOne(booking);
-   res.send(result)
+   // send email about appointment confirmation
+   sendBookingEmail(booking)
+   res.send(result);
   });
 
   app.post('/create-payment-intent', async (req, res) => {
